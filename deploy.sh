b93fddef1d1441ffa8b82a423c003c955bb20e37
Julian Krzefski <krzefski@ksk-agentur.de>
2018-04-04 10:04:09 +0200

    Unifies deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+$1 `which composer` update
+
+BASEDIR=$(dirname "$0")/../../..
+
+$1 $BASEDIR/bin/console sw:plugin:refresh
+$1 $BASEDIR/bin/console sw:plugin:install "${PWD##*/}" --activate
+$1 $BASEDIR/bin/console sw:plugin:update "${PWD##*/}"
+$1 $BASEDIR/bin/console sw:snippets:to:db --include-plugins
+$1 $BASEDIR/bin/console sw:theme:synchronize
+$1 $BASEDIR/bin/console sw:theme:cache:generate
+$1 $BASEDIR/bin/console sw:cache:clear
