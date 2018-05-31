9cd35e7acea95186958773d5eb9dae917f836d9d
Jana Schwalm <Yarisade@users.noreply.github.com>
2018-05-31 02:45:12 +0200

    Fix resize issues

--- Resources/views/frontend/_public/src/js/jquery.unify-height.js
+++ Resources/views/frontend/_public/src/js/jquery.unify-height.js
@@ -1,10 +1,16 @@
 ;(function ($, StateManager) {
     'use strict';
 
+    StateManager.addPlugin('*[data-heptacom-emotion-resizer-unify-height="true"] .emotion--row', 'swPanelAutoResizer', {
+        panelBodySelector: '.heptacom_emotion_resizer_unify_height'
+    });
+
     $.subscribe('plugin/swEmotionLoader/onLoadEmotionFinished', function () {
-        StateManager.addPlugin('*[data-heptacom-emotion-resizer-unify-height="true"] .emotion--row', 'swPanelAutoResizer', {
-            panelBodySelector: '.heptacom_emotion_resizer_unify_height'
-        });
+        StateManager.updatePlugin('*[data-heptacom-emotion-resizer-unify-height="true"] .emotion--row', 'swPanelAutoResizer');
+    });
+
+    StateManager.on('resize', function () {
+        StateManager.updatePlugin('*[data-heptacom-emotion-resizer-unify-height="true"] .emotion--row', 'swPanelAutoResizer');
     });
 
 })(jQuery, window.StateManager);
